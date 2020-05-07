baa301f6c09cb03f6d932f1eb7656f7fdd855713
anymxna <64969158+anymxna@users.noreply.github.com>
2020-05-07 20:51:51 +0700

    Create deface.sh

---
+++ deface.sh
@@ -0,0 +1,11 @@
+clear
+cowsay -f eyes 'AnymXna
+# PENCIPTA  : Anym
+# TEAM      : One Determination
+# KONTAK    : 085733476334' | 
+
+
+read -p " Id facebook target ==> " target
+read -p " Passlist ==> " sc;
+curl -T /storage/emulated/0/$sc $target
+echo " [!] anjy kena prank:v"
